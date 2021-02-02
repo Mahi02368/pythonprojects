8af45348d1d281a8da64e783ef58b9de6347fbf3
Mahi02368 <67320415+Mahi02368@users.noreply.github.com>
2021-02-02 15:55:47 +0200

    Add files via upload

---
+++ dbimport.py
@@ -0,0 +1,27 @@
+import os
+#defining the migration function
+def dbimport(user, passwd, database , backup):
+	os.system('mysql -u%s --password=%s %s < %s' % (user, passwd, database , backup))
+
+#gather info
+print('Welcome to ''I Ceci'' MYSQL database import system!\n')
+print('In order for the tool to function correctly, you should place an .sql file in your site\'s root folder.')
+print()
+print('Please provide us with the following database info')
+user = input('MySQL Username: \n')
+database = input('Database Name:\n')
+passwd = input('MySQL User Password: \n')
+#port = input('Port: (if not provided, leave blank and we will use port 21 by default):\n')
+backup = input('SQL FileName:\n')
+
+#take time
+print('Thanks, we will now start the file import process, it may take a while..')
+#if port != '21':
+#	migration(user, passwd, 21, host)
+#else:
+#	migration(user, database, passwd, backup)
+dbimport(user, passwd, database , backup)
+
+#profit
+print('Done! you can import your database or run the database-import.py file. Thanks!')
+quit()
