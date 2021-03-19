c40e2beb4af3ae98a2757fa59c611e79a49fb40b
Mahi02368 <67320415+Mahi02368@users.noreply.github.com>
2021-03-19 08:58:16 +0100

    Update migration-output.py

--- migration-output.py
+++ migration-output.py
@@ -9,16 +9,14 @@ print('Welcome to ''I Ceci'' migration system!\nIn order to achieve the best pos
 print('The FTP account you are going to use should lead directly to your site root folder, in order to avoid downloading unnecessary files which could take more space then needed.\nTo start, please give us the following information:')
 user = input('FTP Username: \n')
 passwd = input('FTP Password: \n')
-#port = input('Port: (if not provided, leave blank and we will use port 21 by default):\n')
-host = input('FTP Host: (preferably an IP or servername):\n')
+host = input('FTP Host: (IP or servername):\n')
 
 #take time
 print('Thanks, we will now start the file migration process, it may take a while..')
-#if port != '21':
-#	migration(user, passwd, 21, host)
-#else:
-#	migration(user, passwd, port, host)
 migration(user, passwd, host)
+
+#cleanup 
+
 
 #profit
 print('Done! you can import your database or run the database-import.py file. Thanks!')
