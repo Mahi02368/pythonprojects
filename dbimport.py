feba46c8a7737ad0ebb49a530aee88c268c057bd
Mahi89 <67320415+Mahi02368@users.noreply.github.com>
2022-10-21 12:56:19 +0200

    Update dbimport.py

--- dbimport.py
+++ dbimport.py
@@ -4,10 +4,9 @@ def dbimport(user, passwd, database , backup):
 	os.system('mysql -u%s --password=%s %s < %s' % (user, passwd, database , backup))
 
 #gather info
-print('Welcome to ''I Ceci'' MYSQL database import system!\n')
+print("Welcome to Mahi's MYSQL database import system!\n")
 print('In order for the tool to function correctly, you should place an .sql file in your site\'s root folder.')
-print()
-print('Please provide us with the following database info')
+print('Please provide me with the following database info')
 user = input('MySQL Username: \n')
 database = input('Database Name:\n')
 passwd = input('MySQL User Password: \n')
