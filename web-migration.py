617fb81ee3d100ac0f8d59c70c4d1ed658819d57
Mahi89 <67320415+Mahi02368@users.noreply.github.com>
2022-10-21 12:58:01 +0200

    Update and rename migration-output.py to web-migration.py

--- web-migration.py
+++ web-migration.py
@@ -5,7 +5,7 @@ def migration(user, password, host):
 	os.system('wget -brc -o transfer_log.log --ftp-user="%s" --ftp-password=%s ftp://%s' % (user, password, host))
 
 #gather info
-print('Welcome to ''I Ceci'' migration system!\nIn order to achieve the best possible result, you should also place a database backup in your domain\'s public folder.')
+print("Welcome to Mahi's migration system!\nIn order to achieve the best possible result, you should also place a database backup in your domain\'s public folder.")
 print('The FTP account you are going to use should lead directly to your site root folder, in order to avoid downloading unnecessary files which could take more space then needed.\nTo start, please give us the following information:')
 user = input('FTP Username: \n')
 passwd = input('FTP Password: \n')
